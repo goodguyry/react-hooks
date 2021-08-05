956905668217376ec9fa7778ab7b93ca1fba17ad
Ryan Domingue <rychdom+github@gmail.com>
2021-08-05 16:05:25 -0700

    Completes exercise 3, extra credit 1

--- src/exercise/03.js
+++ src/exercise/03.js
@@ -3,11 +3,18 @@
 
 import * as React from 'react'
 
-function Name({name, onNameChange}) {
+function Name() {
+  const [name, setName] = React.useState('');
+
   return (
     <div>
       <label htmlFor="name">Name: </label>
-      <input autocomplete="off" id="name" value={name} onChange={onNameChange} />
+      <input
+        autocomplete="off"
+        id="name"
+        value={name}
+        onChange={(event) => setName(event.target.value)}
+      />
     </div>
   )
 }
@@ -17,6 +24,7 @@ function FavoriteAnimal({ animal, onAnimalChange }) {
     <div>
       <label htmlFor="animal">Favorite Animal: </label>
       <input
+        autocomplete="off"
         id="animal"
         value={animal}
         onChange={(event) => onAnimalChange(event.target.value)}
@@ -25,19 +33,18 @@ function FavoriteAnimal({ animal, onAnimalChange }) {
   )
 }
 
-function Display({name, animal}) {
-  return <div>{`Hey ${name}, your favorite animal is: ${animal}!`}</div>
+function Display({animal}) {
+  return animal ? <div>{`Your favorite animal is: ${animal}!`}</div> : null;
 }
 
 function App() {
-  const [name, setName] = React.useState('');
   const [animal, setAnimal] = React.useState('');
 
   return (
     <form>
-      <Name name={name} onNameChange={(event) => setName(event.target.value)} />
+      <Name />
       <FavoriteAnimal animal={animal} onAnimalChange={setAnimal} />
-      <Display name={name} animal={animal} />
+      <Display animal={animal} />
     </form>
   )
 }
