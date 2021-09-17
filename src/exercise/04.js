8f6cf86ce58b03140348daa912aa0510c36b5754
Ryan Domingue <rychdom+github@gmail.com>
2021-09-17 12:28:11 -0700

    Completes exercise 4, extra credit 2

--- src/exercise/04.js
+++ src/exercise/04.js
@@ -2,16 +2,11 @@
 // http://localhost:3000/isolated/exercise/04.js
 
 import * as React from 'react'
+import { useLocalStorageState } from '../utils';
 
 function Board() {
   // 🐨 squares is the state for this component. Add useState for squares
-  const [squares, setSquares] = React.useState(() =>
-    JSON.parse(window.localStorage.getItem('squares')) || Array(9).fill(null)
-  );
-
-  React.useEffect(() => {
-    window.localStorage.setItem('squares', JSON.stringify(squares))
-  }, [squares])
+  const [squares, setSquares] = useLocalStorageState('squares', Array(9).fill(null));
 
   // 🐨 We'll need the following bits of derived state:
   // - nextValue ('X' or 'O')
